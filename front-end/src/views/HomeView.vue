e3426d8c5a34b13c8c990300fbf693a73c5d1b2f
Héctor Rodríguez <hd.rdz98@gmail.com>
2022-10-01 14:16:18 -0500

    Initial MoonView

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-3xl font-bold underline text-blue-600 cursor-pointer transition-all hover:text-blue-300">
+    Home
+  </h1>
+  <router-link :to="{ 'name': 'moon' }">
+    Moon
+  </router-link>
 </template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
